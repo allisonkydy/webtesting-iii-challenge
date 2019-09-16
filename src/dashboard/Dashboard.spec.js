8cc76fcde4e7a8330ae097dbcb0d88fdc43a9d6f
allisonkydy <allisnelly@gmail.com>
2019-09-16 14:38:20 -0500

    upgraded dependencies, wrote dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,26 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from '../dashboard/Dashboard';
+
+test('dashboard renders without crashing', () => {
+  render(<Dashboard />);
+});
+
+test('dashboard matches snapshot', () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+test('dashboard gate displaying, defaulting to unlocked and open', () => {
+  const { getByText } = render(<Dashboard />);
+
+  getByText(/unlocked/i);
+  getByText(/open/i);
+});
+
+test('dashboard controls displaying', () => {
+  const { getByText } = render(<Dashboard />);
+
+  getByText(/lock gate/i);
+  getByText(/close gate/i);
+});
