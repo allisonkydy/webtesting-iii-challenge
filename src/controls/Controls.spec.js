6521af51072c688c7380d40c862a0803a6a51b1f
allisonkydy <allisnelly@gmail.com>
2019-09-16 17:23:36 -0500

    tested button text in different states, MVP complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,36 +1,50 @@
-import React from 'react';
-import { render, fireEvent, getByText } from '@testing-library/react';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-import Controls from '../controls/Controls';
+import Controls from "../controls/Controls";
 
-test('Controls renders without crashing', () => {
+test("Controls renders without crashing", () => {
   render(<Controls />);
-})
+});
 
-test('Controls provides buttons to toggle the closed and locked states', () => {
-  const { getByText } = render(<Controls />);
+test("Controls provides buttons to toggle the closed and locked states", () => {
+  const { getByText } = render(<Controls locked={false} closed={false} />);
 
   getByText(/lock gate/i);
   getByText(/close gate/i);
-})
+});
 
-// test("Closed toggle button's text changes when clicked", () => {
-//   let closed = false;
-//   const toggleClosed = jest.fn(() => closed = true);
-//   const { getByText } = render(<Controls closed={closed} toggleClosed={toggleClosed} />);
+test("Closed toggle button text changes to open when clicked", () => {
+  const { getByText } = render(<Controls locked={false} closed={true} />);
 
-//   const closeButton = getByText(/close gate/i);
-//   fireEvent.click(closeButton);
-//   expect(toggleClosed).toHaveBeenCalled();
-//   getByText(/open gate/i);
-// })
+  getByText(/open gate/i);
+});
 
-test('Closed toggle button is disabled if gate is locked', () => {
+test("Closed toggle button text changes to close when clicked", () => {
+  const { getByText } = render(<Controls locked={false} closed={false} />);
+
+  getByText(/close gate/i);
+});
+
+test("Locked toggle button text changes to unlock when clicked", () => {
+  const { getByText } = render(<Controls locked={true} closed={true} />);
+
+  getByText(/unlock gate/i);
+});
+
+test("Locked toggle button text changes to lock when clicked", () => {
+  const { getByText } = render(<Controls locked={false} closed={true} />);
+
+  getByText(/lock gate/i);
+});
+
+test("Closed toggle button is disabled if gate is locked", () => {
   const { getByText } = render(<Controls locked={true} closed={true} />);
   expect(getByText(/open gate/i).disabled).toBe(true);
-})
+});
 
-test('Locked toggle button is disabled if gate is open', () => {
+test("Locked toggle button is disabled if gate is open", () => {
   const { getByText } = render(<Controls locked={false} closed={false} />);
   expect(getByText(/lock gate/i).disabled).toBe(true);
-})
+});
